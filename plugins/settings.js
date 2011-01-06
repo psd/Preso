f74fc3a72a6ca5fbd16c8777cc99c94365c6d06c
Paul Downey <pauldowney@Paul-Downeys-Macbook-Air.local>
2011-01-06 10:16:41 +0000

    default map

--- plugins/settings.js
+++ plugins/settings.js
@@ -4,4 +4,5 @@ config.animDurationFade = 2000;
 
 config.options.txtTickerMinInterval = 10;
 config.options.txtFadingMessagesTimeout = 0;
-config.options.txtCecilyMap = "ConfusedMap";
+config.options.txtCecilyMap = "MyMap";
+config.options.chkCecilyAutoSaveMap = false;
